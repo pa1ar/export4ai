b905844c418ce214ac9e8c7a065db78e96b3e7a8
pa1ar <pavel@1ar.io>
2024-08-14 17:50:13 +0200

    added -h support, fixed exclusion of folders

--- export4ai.py
+++ export4ai.py
@@ -1,22 +1,30 @@
 import os
 import argparse
+import sys
 
 def parse_arguments():
-  parser = argparse.ArgumentParser(description='Export files content with optional folder exclusion.')
+  parser = argparse.ArgumentParser(description='Export files content in a folder with optional folder exclusion.')
   parser.add_argument('folders', nargs='*', default=['.'], help='Folders to include. Defaults to the current directory.')
   parser.add_argument('-x', '--exclude', nargs='+', help='Excluded folders')
+
+  # Show help if no arguments are provided
+  if len(sys.argv) == 1:
+    parser.print_help(sys.stderr)
+    sys.exit(1)
+  
   return parser.parse_args()
 
 def get_directory_content(folder_list, excluded_folders):
+  excluded_folders = [os.path.abspath(f) for f in excluded_folders]  # Convert to absolute paths
   content = ""
   for folder_path in folder_list:
-    if folder_path in excluded_folders:
-      continue
     for root, dirs, files in os.walk(folder_path):
       # Remove excluded folders from dirs
-      dirs[:] = [d for d in dirs if os.path.join(root, d) not in excluded_folders]
+      dirs[:] = [d for d in dirs if os.path.abspath(os.path.join(root, d)) not in excluded_folders]
       for file in files:
         file_path = os.path.join(root, file)
+        if any(os.path.abspath(root).startswith(excluded_folder) for excluded_folder in excluded_folders):
+          continue  # Skip files in the excluded folders
         if file.endswith(('.txt', '.py', '.swift', '.js', '.html', '.css', '.json', '.xml', '.yaml', '.yml', '.md', '.entitlements', '.plist', '.ini', '.cfg', '.config', '.env', '.xcodeproj', '.xcworkspace', '.toml', '.editorconfig', '.csv', '.log', '.gradle', '.pom', '.gitignore', '.gitattributes', '.gitmodules', '.patch', '.test', '.spec', '.rst', '.doctest', '.php', '.jsp', '.asp', '.ejs', '.vue', '.jsx', '.tsx', '.scss', '.sass', '.less', '.sh', '.bat', '.svg')):
           with open(file_path, 'r') as f:
             file_content = f.read()
